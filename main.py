c665c0794c54ad6fc0c654b22577e232ca520fe5
Varga Lilla <vargalilla@ktch.hu>
2023-12-20 10:12:44 +0100

    Hatvani dolgozók fizetése kiszámolva

---
+++ main.py
@@ -0,0 +1,52 @@
+# Az alkalmazás itt fut
+
+from dolgozo import Dolgozo
+
+class Ber:
+    # konstruktor
+    # az osztályváltozóknak kezdő értéket adunk
+    # adattagok előkészítése
+    def __init__(self):
+        self.filename = "berkft.txt"
+        self.dolgozoLista = []
+        
+    # fájl beolvasása
+    def olvas_fajl(self):
+        fp = open(self.filename, "r", encoding="utf-8")
+        lines = fp.readlines()
+        fp.close()
+        for line in lines:
+            line = line.rstrip() #levesszük jobb oldalról a sortörést
+            (nev, telepules, cim, szuletes, fizetes) = line.split(":")
+            dolgozo = Dolgozo(nev, telepules, cim, szuletes, fizetes)
+            self.dolgozoLista.append(dolgozo)
+            print(dolgozo.fizetes)
+    # Szolnokiak
+    def szolnoki(self):
+        szolnokiLista = []
+        for dolgozo in self.dolgozoLista:
+            if dolgozo.telepules == "Szolnok":
+                szolnokiLista.append(dolgozo)
+        #for vége
+        #legfiatalabb; minimumkiválasztás tétel
+        max_szolnoki = szolnokiLista[0]
+        for szolnoki in szolnokiLista:
+            if szolnoki.szuletes > max_szolnoki.szuletes:
+                max_szolnoki = szolnoki
+        
+        print(max_szolnoki.szuletes)
+    
+    # Hatvani dolgozók fizetése:        
+    def hatvani_fizetes(self):
+        osszeg = 0
+        for dolgozo in self.dolgozoLista:
+            if dolgozo.telepules == "Hatvan":
+                osszeg = osszeg + int(dolgozo.fizetes)
+            print("Hatvaniak fizetése:",osszeg)
+            
+# class Ber vége
+
+ber = Ber()
+ber.olvas_fajl()
+ber.szolnoki()
+ber.hatvani_fizetes()
